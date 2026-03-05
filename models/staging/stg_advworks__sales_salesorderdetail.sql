3b2732f39cc3c027528f3d25f8de718fce3376b2
Mauro Tessarolo Junior <maurotessa@gmail.com>
2026-03-05 18:08:12 +0000

    feature/dbuilt_package_added

--- models/staging/stg_advworks__sales_salesorderdetail.sql
+++ models/staging/stg_advworks__sales_salesorderdetail.sql
@@ -4,23 +4,20 @@ with source as (
 
 renamed as (
     select
-        -- Primary Key
+        {{ dbt_utils.generate_surrogate_key(['SalesOrderID', 'ProductID']) }} as sales_order_detail_sk,
+        
         cast(SalesOrderDetailID as bigint) as sales_order_detail_pk,
         
-        -- Foreign Keys
         cast(SalesOrderID as bigint) as sales_order_fk,
         cast(ProductID as bigint) as product_fk,
         cast(SpecialOfferID as bigint) as special_offer_fk,
         
-        -- Order Details
         cast(OrderQty as bigint) as order_quantity,
         cast(UnitPrice as double) as unit_price,
         cast(UnitPriceDiscount as double) as unit_price_discount,
         
-        -- Calculated field
         cast(UnitPrice as double) * cast(OrderQty as bigint) * (1 - cast(UnitPriceDiscount as double)) as line_total,
         
-        -- System columns
         cast(rowguid as string) as rowguid,
         cast(ModifiedDate as date) as last_updated_at
         
